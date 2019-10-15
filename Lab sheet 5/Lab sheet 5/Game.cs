2c50f1135531c59db8398d5b9b29470fe4b8c9b9
David Coleman <46888854+S00189824@users.noreply.github.com>
2019-10-15 14:00:27 +0100

    Part 1 and 2 Finished

---
+++ Lab sheet 5/Lab sheet 5/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_5
+{
+    class Game
+    {
+        string name { get; set; }
+        decimal price { get; set; }
+        DateTime ReleaseDate { get; set; }
+
+        public Game (string Name,decimal Price,DateTime releaseDate)
+        {
+            name = Name;
+            price = Price;
+            ReleaseDate = releaseDate;
+
+
+        }
+
+        public Game(string name, decimal price) : this(name, price, DateTime.Now) { }
+
+        public Game(string name):this(name, 0)
+        {
+
+        }
+    }
+}
---
+++ Lab sheet 5/Lab sheet 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g1 = new Game("COD", 60.00m,new DateTime(04,11,2019));
+            Game g2 = new Game("Astellia Online", 10.00m,new DateTime(25,08,2019));
+
+            Game g3 = new Game("Final Fantasy XIV");
+
+            Game g4 = new Game("GTA", 10.99m);
+        }
+    }
+}
